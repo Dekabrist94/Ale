e4bf0372fbc5a7bf232f600274eed8559f9a8b38
Dekabrist94 <dekabriskii94@gmail.com>
2024-01-04 12:54:54 +0100

    first commit

---
+++ src/Prodotti.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container} from 'react-bootstrap';
+
+export const Prodotti = () => {
+  return <>
+  <Container style={{paddingTop:'25rem'}}>
+   <ul>
+    <li>
+    cosa faciamo (Automotive, Pneumatica, Oliodinamica e strumentazione varia )
+    </li>
+    <li>
+     
+    </li>
+    <li>
+      
+    </li>
+    <li>
+    
+    </li>
+   </ul>
+  </Container>
+  </>;
+};
---
+++ src/Technologia.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container} from 'react-bootstrap';
+
+export const Technologia = () => {
+  return <>
+  <Container style={{paddingTop:'25rem'}}>
+   <ul>
+    <li>
+pezzi
+    </li>
+    <li>
+    macchinari e strumenti di misura 
+    </li> 
+    <li>
+      contatore di pezzi +2000
+    </li>
+    <li>
+     
+    </li>
+   </ul>
+  </Container>
+  </>;
+};
